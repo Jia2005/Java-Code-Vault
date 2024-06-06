389230391cfe137b99192ae613cb7f92b4c8a781
Jia2005 <143592429+Jia2005@users.noreply.github.com>
2024-06-06 11:54:29 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -4,12 +4,12 @@ class add
 {
  public static void main(String a1[])
  {
- Scanner sc= new Scanner(System.in);
- System.out.println("Input the value of a");
- int a=sc.nextInt();
- System.out.println("Input the value of b");
- int b=sc.nextInt();
- int c=a+b; 
- System.out.println ("The addition of a and b is "+c);
+     Scanner sc= new Scanner(System.in);
+     System.out.println("Input the value of a");
+     int a=sc.nextInt();
+     System.out.println("Input the value of b");
+     int b=sc.nextInt();
+     int c=a+b; 
+     System.out.println ("The addition of a and b is "+c);
  }
 }
