b501ba7fd334506f22ab3fcb5e7e5b6e829a8de0
Jia Harisinghani <143592429+Jia2005@users.noreply.github.com>
2025-05-11 00:38:00 +0530

    Update Payment Exception (User-defined).java

--- Payment Exception (User-defined).java
+++ Payment Exception (User-defined).java
@@ -1,40 +1,32 @@
 //Pay out of Bound
 import java.util.*;
-class PayOutOfBounds extends Exception
-{
-PayOutOfBounds(String str)
-{
-super(str);
+
+class PayOutOfBounds extends Exception {
+    PayOutOfBounds(String str) {
+        super(str);
+    }
 }
-}
-class Handling
-{
-public static void main(String a[]) throws PayOutOfBounds
-{
-boolean continueInput=true;
-do
-{
-try
-{
-Scanner sc = new Scanner(System.in);
-System.out.println("\nEnter the Payment of Superintendent");
-int s = sc.nextInt();
-if(s>=25000 && s<=50000)
-{
-System.out.println("Superintendent is paid");
-}
-else
-{
- PayOutOfBounds pay = new PayOutOfBounds("Payment Out Of Bound");
-throw pay;
-}
-continueInput=false;
-}
-catch(PayOutOfBounds pay)
-{
-System.out.println("Exception is "+pay);
-}
-}
-while(continueInput);
-}
+
+class Handling {
+    public static void main(String a[]) throws PayOutOfBounds {
+        boolean continueInput = true;
+        do {
+            try {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("\nEnter the Payment of Superintendent");
+                int s = sc.nextInt();
+                
+                if(s >= 25000 && s <= 50000) {
+                    System.out.println("Superintendent is paid");
+                } else {
+                    PayOutOfBounds pay = new PayOutOfBounds("Payment Out Of Bound");
+                    throw pay;
+                }
+                continueInput = false;
+            }
+            catch(PayOutOfBounds pay) {
+                System.out.println("Exception is " + pay);
+            }
+        } while(continueInput);
+    }
 }
