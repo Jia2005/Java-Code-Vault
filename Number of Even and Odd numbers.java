0ec3f73e1ec1f3b38c6739322abafd27aecaa2a3
Jia Harisinghani <143592429+Jia2005@users.noreply.github.com>
2025-05-11 00:29:31 +0530

    Update Number of Even and Odd numbers.java

--- Number of Even and Odd numbers.java
+++ Number of Even and Odd numbers.java
@@ -1,27 +1,27 @@
 //Number of even and odd numbers
 import java.io.*;
 import java.util.*;
-class Even
-{
-public static void main(String a[])
-{
-int Even=0,Odd=0,i;
-int[] Numbers = new int[50];
-Scanner sc = new Scanner(System.in);
-System.out.println("Enter the number of inputs");
-int num = sc.nextInt();
-for(i=0;i<num;i++)
-{
-Numbers[i]=sc.nextInt();
-}
-for(i=0;i<num;i++)
-{
-if(Numbers[i]%2==0)
-Even++;
-else
-Odd++;
-}
-System.out.println("The number of even numbers are "+Even+" and the number of 
-odd numbers are "+Odd);
-}
+
+class Even {
+    public static void main(String a[]) {
+        int Even = 0, Odd = 0, i;
+        int[] Numbers = new int[50];
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the number of inputs");
+        int num = sc.nextInt();
+        
+        for(i = 0; i < num; i++) {
+            Numbers[i] = sc.nextInt();
+        }
+        
+        for(i = 0; i < num; i++) {
+            if(Numbers[i] % 2 == 0)
+                Even++;
+            else
+                Odd++;
+        }
+        
+        System.out.println("The number of even numbers are " + Even + " and the number of odd numbers are " + Odd);
+    }
 }
